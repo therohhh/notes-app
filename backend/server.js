b18ecf0744aa55ab89ef7f6950ee3bd4222467ee
therohhh <rohithlenka00@gmail.com>
2026-03-11 19:50:41 +0530

    Connected MongoDB Atlas and added dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
 const PORT = 5000;
 app.listen(PORT,()=>{
