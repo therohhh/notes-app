9344b9ab80403872c55670acf79e226c8cf0f5f0
therohhh <rohithlenka00@gmail.com>
2026-03-12 11:05:51 +0530

    added crud routes and tested with postman

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const Note = require("./models/Note");
 
 dotenv.config();
 app.use(cors());
@@ -19,4 +20,51 @@ app.listen(PORT,()=>{
 
 app.get("/",(req,res)=>{
     res.send("Notes API is running.....")
+});
+
+app.post("/notes",async(req, res)=>{
+    try{
+        const {title,content} = req.body;
+        const note = new Note({
+            title,
+            content
+        });
+        const savedNote = await note.save();
+        res.status(201).json(savedNote);
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+});
+app.get("/notes", async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.json(notes);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+app.put("/notes/:id", async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        const updatedNote = await Note.findByIdAndUpdate(
+            req.params.id,
+            { title, content },
+            { new: true }
+        );
+
+        res.json(updatedNote);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+app.delete("/notes/:id", async (req, res) => {
+    try {
+        await Note.findByIdAndDelete(req.params.id);
+        res.json({ message: "Note deleted" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
